ee49baacba503dc0096f83ef63c0cd52044775cb
nayler <msnayler1@gmail.com>
2025-11-19 22:15:22 -0500

    tercerroooo

--- .next/dev/static/chunks/iptv_app_layout_tsx_84591206._.js
+++ .next/dev/static/chunks/iptv_app_layout_tsx_84591206._.js
@@ -2,7 +2,7 @@
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
   "static/chunks/[root-of-the-server]__8be13ccc._.css",
-  "static/chunks/91524__pnpm_53efdc15._.js",
+  "static/chunks/91524__pnpm_c63b6b3e._.js",
   "static/chunks/iptv_93934484._.js"
 ],
     source: "dynamic"
